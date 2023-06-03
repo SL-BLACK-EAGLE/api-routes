2a044e39e0a42f1e87f6bf19812812ddd24c3a72
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-06-03 19:57:49 +0530

    change Main page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const page = async () => {
   await wait (5000)
-  const  res   = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const  res   = await fetch('https://jsonplaceholder.typicode.com/posts/')
   const data =  await res.json()
 
   return <div>{JSON.stringify(data)}</div>;
